4d32ff66e5ee006bd99aab5d9064bd8d46099376
Claudio Albano Guimaraes <albano.guimaraes@gmail.com>
2025-03-24 16:49:57 -0400

    feat: order file by name

--- common.py
+++ common.py
@@ -5,7 +5,7 @@ from PyPDF2 import PdfWriter, PdfReader
 def merge_files():
     merger = PdfWriter()
 
-    for filename in glob("files/merge/*.pdf", recursive=True):
+    for filename in sorted(glob("files/merge/*.pdf", recursive=True)):
         merger.append(filename)
 
     dt = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
@@ -17,7 +17,7 @@ def merge_files():
     return file_merged
 
 def split_files(start_page: int, end_page):
-    for filename in glob("files/split/*.pdf", recursive=True):
+    for filename in sorted(glob("files/split/*.pdf", recursive=True)):
         pdf = PdfReader(filename)
 
     merger = PdfWriter()
